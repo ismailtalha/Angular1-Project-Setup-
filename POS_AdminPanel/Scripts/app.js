b186aa0d3563ad6ef77eddd3e50f3280b62d6366
ISMAILTALHA <ismail.talha@outlook.com>
2018-12-12 14:16:12 +0500

    initail commit

---
+++ POS_AdminPanel/Scripts/app.js
@@ -0,0 +1,49 @@
+﻿var app = angular.module('POS', ['ui.router']);
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/auth/Login');
+    $stateProvider
+			.state('auth', {
+			    abstract: true,
+			    module: 'public',
+			    url: '/auth',
+			    template: '<div ui-view class="scroll_bar" ></div>'
+			})
+         .state('auth.Login', {
+             url: '/Login',
+             controller: 'LoginCtrl',
+             templateUrl: 'Pages/Login/LoginPage.html'
+         })
+        .state('app', {
+            abstract: true,
+            url: '/app',
+            templateUrl: 'app.html'
+        })
+		.state('app.Home', {
+		    url: '/Home',
+		    controller: 'HomeCtrl',
+		    templateUrl: 'Pages/Home/HomePage.html',
+
+		})
+        .state('app.secondpage', {
+            url: '/secondpage',
+            controller: 'SecondPageCtrl',
+            templateUrl: 'Pages/SecondPage/SecondPage.html',
+
+        })
+        	
+
+		
+	
+});
+    "use strict";
+    app.controller("HomeCtrl", function ($scope, $state, $rootScope, $http) {
+      
+    })
+    app.controller("SecondPageCtrl", function ($scope, $state, $rootScope, $http) {
+       
+    })
+    app.controller("LoginCtrl", function ($scope, $state, $rootScope, $http) {
+       
+    })
+
---
+++ POS_AdminPanel/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(POS_AdminPanel.Startup))]
+namespace POS_AdminPanel
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
